878507224b97d78e9d4b585b533cb4342b1c0585
Sampashree Nayak <31867379+sampanayak@users.noreply.github.com>
2019-02-06 22:13:37 -0600

    Add files via upload

--- URLtoIPaddress.py
+++ URLtoIPaddress.py
@@ -1,21 +1,12 @@
-#urls to keywords
-import csv
+import socket
 from urllib.parse import urlparse
-
 #contents = []
-f = open('urls.txt', 'r')
-urls = f.read()
-print(urls)
-for url in urls:
-#		contents.append(url)
-	url_keyword = urlparse(url)
-print(url_keyword)
-f.close()		
-
-
-#output
-#ParseResult(scheme='http', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html', params='', query='', fragment='')
-
-#import socket
-#print(socket.gethostbyname('google.com'))
-
+with open('urls.txt', 'r') as filehandle:
+	for line in filehandle:
+		o = urlparse(line)
+		#print(o)
+		print(o.netloc)
+		print(socket.gethostbyname(o.netloc))
+		
+filehandle.close()
+		
