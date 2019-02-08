7c7b01f18c0341647fa752649d26ecd9f3464a3a
Sampashree <samnayak1989@gmail.com>
2019-02-08 11:53:34 -0600

    commit

--- URLtoIPaddress.py
+++ URLtoIPaddress.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 with open('urls.txt', 'r') as filehandle:
 	for line in filehandle:
 		o = urlparse(line)
-		#print(o)
+		print(o)
 		print(o.netloc)
 		print(socket.gethostbyname(o.netloc))
 		
--- gene.py
+++ gene.py
@@ -1,3 +1,27 @@
+contents = []
+import re
+i = 0
 with open('gene.txt') as f:
 	lines = f.readlines()
-print(lines)
+	i = 0
+	sizeOflines = len(lines)
+	while i < sizeOflines:
+		print(lines[i])
+		i += 1
+		for match in re.finditer(lines[i+2], lines[i]):
+			print(match.start())
+		
+	#for i in lines:
+		#print(lines[i])
+		#i == i++
+	#i = 1
+	#print(lines[1])
+	
+	#for i in lines:
+		#print(lines[i++])
+		#[ m.start() for m in re.finditer(lines[i++], lines[i])]
+		#for match in re.finditer(lines[2], lines[1]):
+			#print(match.start())
+		
+	
+	
