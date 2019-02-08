34e67de54b3be9db81f54b6ab3539f295f7ed287
Sampashree Nayak <31867379+sampanayak@users.noreply.github.com>
2019-02-08 14:22:50 -0600

    Update gene.py

--- gene.py
+++ gene.py
@@ -22,6 +22,26 @@ with open('gene.txt') as f:
 		#[ m.start() for m in re.finditer(lines[i++], lines[i])]
 		#for match in re.finditer(lines[2], lines[1]):
 			#print(match.start())
-		
+
+			
+			#contents = []
+import re
+#i = 0
+with open('gene.txt') as f:
+    lines = f.readlines()
+    i = 1
+    sizeOflines = len(lines)
+    while i < sizeOflines:
+        # print(lines[i])
+        fullString = lines[i]
+        i += 1
+        sub = lines[i]
+        print(fullString)
+        print(sub)
+        i += 1
+        for match in re.finditer('sub','fullString'):
+            print(match.start())
+        #[m.start() for m in re.finditer('sub', 'fullString')]
+
 	
 	
